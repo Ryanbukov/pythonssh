93d3eac5df9e6294fd9ddf596bd2b2d4068aa6e1
ryanbukov <ryanbukov58@mail.ru>
2020-09-30 09:24:45 -0700

    The second version

--- sshs.py
+++ sshs.py
@@ -1,24 +1,14 @@
 from pexpect import pxssh
 
-try:
-    s = pxssh.pxssh()
-    hostname = '192.168.253.128'
-    username = 'rushan'
-    password = 'root'
-    s.login(hostname, username, password)
-    s.sendline('uptime')
-    s.prompt()
-    print(s.before)
-    s.sendline('su')
-    s.prompt()
-    print(s.before)
-    s.sendline('root')
-    s.prompt()
-    print(s.before)
-    s.sendline('sudo ifconfig ens33 192.168.1.18 netmask 255.255.255.0')
-    s.prompt()
-    print(s.before)
+s = pxssh.pxssh()
+hostname = '192.168.253.128'
+username = 'rushan'
+password = 'root'
+s.login(hostname, username, password)
 
-except pxssh.ExceptionPxssh as e:
-    print("pxssh failed on login.")
-    print(e)
+s.sendline('su')
+s.sendline('root')
+s.sendline('sudo ifconfig ens33 192.168.1.15 netmask 255.255.255.0')
+
+s.prompt()
+print(s.before)
