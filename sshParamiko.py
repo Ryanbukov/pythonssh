7d9cfc386873174be69f99eb4e4c3c12ec26a14e
ryanbukov <ryanbukov58@mail.ru>
2020-10-04 07:33:20 -0700

    ssh v2 add on paramiko

--- sshParamiko.py
+++ sshParamiko.py
@@ -1,33 +1,19 @@
 import paramiko
-import time
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect("192.168.253.128", username="rushan", password="root", port=22)
-chan = ssh.invoke_shell()
-#command = "TERM=xterm " + "su"
-#TERM_WIDTH = 'terminal width 250\n'
 
 stdin, stdout, stderr = ssh.exec_command("uptime")
-stdin.flush()
 result = stdout.read().splitlines()
 print(result)
 
-stdin, stdout, stderr = ssh.exec_command('sudo ifconfig ens33 192.168.1.14 netmask 255.255.255.0', get_pty = True)
-#time.sleep(7)
-#chan.send(TERM_WIDTH)
-stdin.write("root" + "\n")
-stdin.flush()
+stdin, stdout, stderr = ssh.exec_command("sudo ifconfig ens33 192.168.1.14 netmask 255.255.255.0", get_pty = True)
+stdin.write("root\n")
 result = stdout.read().splitlines()
 print(result)
-
-#stdin, stdout, stderr = ssh.exec_command("ip a add 192.168.1.14/24 dev ens33")
-#stdin.flush()
-#result = stdout.read().splitlines()
-#print(result)
 
 stdin, stdout, stderr = ssh.exec_command("ip a")
-stdin.flush()
 result = stdout.read().splitlines()
 print(result)
 ssh.close()
--- sshs.py
+++ sshs.py
@@ -6,9 +6,9 @@ username = 'rushan'
 password = 'root'
 s.login(hostname, username, password)
 
-s.sendline('su')
-s.sendline('root')
 s.sendline('sudo ifconfig ens33 192.168.1.15 netmask 255.255.255.0')
+s.sendline('root')
+s.sendline('ip a')
 
 s.prompt()
 print(s.before)
