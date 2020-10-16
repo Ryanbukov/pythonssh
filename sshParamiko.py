cecc1ca5455cb3a1d911dcc48da423b4666a35d0
ryanbukov <ryanbukov58@mail.ru>
2020-10-16 12:59:30 -0700

    ssh v3

--- sshParamiko.py
+++ sshParamiko.py
@@ -8,12 +8,8 @@ stdin, stdout, stderr = ssh.exec_command("uptime")
 result = stdout.read().splitlines()
 print(result)
 
-stdin, stdout, stderr = ssh.exec_command("sudo ifconfig ens33 192.168.1.14 netmask 255.255.255.0", get_pty = True)
+stdin, stdout, stderr = ssh.exec_command("sudo ifconfig ens33:0 192.168.1.17 netmask 255.255.255.0", get_pty = True)
 stdin.write("root\n")
-result = stdout.read().splitlines()
-print(result)
-
-stdin, stdout, stderr = ssh.exec_command("ip a")
 result = stdout.read().splitlines()
 print(result)
 ssh.close()
--- sshs.py
+++ sshs.py
@@ -6,9 +6,13 @@ username = 'rushan'
 password = 'root'
 s.login(hostname, username, password)
 
-s.sendline('sudo ifconfig ens33 192.168.1.15 netmask 255.255.255.0')
+s.sendline('sudo ifconfig ens33:0 192.168.1.21 netmask 255.255.255.0')
+s.prompt()
+print(s.before)
 s.sendline('root')
-s.sendline('ip a')
+s.prompt()
+print(s.before)
 
+s.sendline('ip a')
 s.prompt()
 print(s.before)
